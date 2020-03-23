a588e8238cab6870ada2bf46776139d698431b48
VietNT <viet_nguyen@t-rnd.me>
2020-03-23 20:32:58 +0900

    django upload file

---
+++ file_uploader/views.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render
+from django.db import models
+from django.http import HttpResponse
+from .forms import UploadFileForm
+ 
+def fileUploaderView(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            upload(request.FILES['file'])
+            return HttpResponse("<h2>File uploaded successful!</h2>")
+        else:
+            return HttpResponse("<h2>File uploaded not successful!</h2>")
+ 
+    form = UploadFileForm()
+    return render(request, 'fileUploaderTemplate.html', {'form':form})
+  
+def upload(f): 
+    file = open(f.name, 'wb+') 
+    for chunk in f.chunks():
+        file.write(chunk)
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^polls/', include('polls.urls')),
     url(r'^user-auth/', include('user_auth.urls')),
+    url(r'^file-upload/', include('file_uploader.urls'))
 ]
